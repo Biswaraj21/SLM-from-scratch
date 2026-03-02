5a75658f3df8ceb4b12832bd87f5432336bbd48a
Biswaraj B <biswarajbhattacharyya@gmail.com>
2026-03-03 00:18:06 +0530

    Remove dataset from repository

--- data/download_tinystories.py
+++
@@ -1,7 +0,0 @@
-from datasets import load_dataset
-
-dataset=load_dataset("roneneldan/TinyStories")
-
-with open("data/tinystories.txt",'w',encoding='utf-8') as f:
-    for example in dataset["train"]:
-        f.write(example['text']+'\n')
--- data/prepare_data.py
+++
@@ -1,5 +0,0 @@
-with open("data/tinystories.txt","r",encoding="utf-8") as f:
-    text=f.read()
-    
-print("Dataset Length:", len(text))
-print(text[:500])
